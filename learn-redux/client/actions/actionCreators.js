af2443b0b41410cf282d049bf93af183b012eda7
dennellmarie <dennell.lucero@gmail.com>
2017-01-09 11:51:04 -0800

    components and actions first draft, store created

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,28 @@
+// increment
+export function incrementf(index) {
+	return {
+		type: 'INCREMENT_LIKES',
+		index
+	}
+}
+
+// add comment
+export function addComment(postId, author, commment) {
+	return {
+		type: 'ADD_COMMENT',
+		postId,
+		author,
+		comment
+	}
+}
+
+// remove comment
+export function removeComment(postId, i) {
+	return {
+		type: 'REMOVE_COMMENT',
+		i,
+		postId
+	}
+}
+
+
