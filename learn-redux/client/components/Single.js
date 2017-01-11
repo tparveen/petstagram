c04f00fec79e6920a646dbda4953312f9913b62a
dennellmarie <dennell.lucero@gmail.com>
2017-01-11 09:32:44 -0800

    working on components, Photo.js route not working properly

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
 
 const Single = React.createClass({
 	render() {
+		// index of the post
+		const i = this.props.posts.findIndex((post) => post.code === 
+				this.props.params.postId);
+		const post = this.props.posts[i]
+		console.log(i);
+		// get us the post
 		return (
 			<div className="single-photo">
-				I'm the single
+				<Photo i={i} post={post} {...this.props} />
 			</div>
 		)
 	}
 });
 
 export default Single;
+
